7204c5fbda014ee15fb307293abe95bad4d8c8b2
RoberdanSantos <roberdan.feitosa@sysmap.com.br>
2025-04-14 16:20:56 -0300

    feat: adding auth and user modules to app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,13 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WeatherModule } from './weather/weather.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    WeatherModule
+    WeatherModule,
+    AuthModule,
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
