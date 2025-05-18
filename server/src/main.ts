052617cad6e6ee830e58d62055132657a5c92a06
RoberdanSantos <roberdan.feitosa@sysmap.com.br>
2025-05-18 19:56:28 -0300

    refactor(core): add Swagger setup, validation pipe and extended CORS config

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,17 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
-    methods: 'GET, POST, PUT, DELETE',
-    credentials: true
+    origin: 'http://localhost:5173',
+    methods: 'GET, POST, PUT, DELETE, PATCH',
+    credentials: true,
   });
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('Weather App')
+    .setDescription('Documentação da API do WeatherApp')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/doc', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
