35e4c72ad02e0e1f58b50ced601e88cf4f278cf9
RoberdanSantos <roberdan.feitosa@sysmap.com.br>
2025-05-18 20:04:44 -0300

    refactor(root): wrap app with ToastProvider and remove StrictMode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './pages/App'
-import './index.css';
+import { createRoot } from "react-dom/client";
+import App from "./pages/App";
+import { ToastProvider } from "./context/ToastContext";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <ToastProvider>
     <App />
-  </StrictMode>,
-)
+  </ToastProvider>,
+);
